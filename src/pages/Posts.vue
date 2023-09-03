fcc60c1f20740d81b156997a1ad2ec2bb28d334b
Ivan <spitfy@mail.ru>
2023-09-03 21:32:56 +0700

    init commit

---
+++ src/pages/Posts.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+      <q-list bordered separator>
+          <h2 style="margin-top: 50px">POSTS</h2>
+          <q-item clickable v-ripple v-for="post in posts" :key="post.id">
+              <q-item-section>
+                  <q-item-label>{{ post.title }}</q-item-label>
+                  <q-item-label caption>{{ post.body }}</q-item-label>
+              </q-item-section>
+          </q-item>
+      </q-list>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'IndexPage',
+    data() {
+        return {
+            posts: [],
+        }
+    },
+    created() {
+        this.$axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.posts = response.data
+            })
+    }
+})
+</script>
